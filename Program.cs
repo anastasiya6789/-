aff02fa5838aedbf5c334bb41c8f480d8232e75d
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-19 12:40:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int n = Convert.ToInt32(Console.ReadLine()), s = 4, i = 1;
+
+            while (i <= n)
+                i += s++;
+            Console.WriteLine(i);
+        }
+    }
+}
