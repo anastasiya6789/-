784b67a72412de1779f874d6c33fe2272e135ee4
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-27 17:18:16 +0300

    Add files via upload

---
+++ 8 задача/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            int sum = 0;
+            while (N > 0)
+            {
+                if (N % 2 == 0)
+                {
+                    count++;
+                    sum += N;
+                }
+
+                N -= 1;
+            }
+
+            Console.WriteLine("Количество парных {0}; Сумма парных: {1}", count, sum);
+
+            Console.ReadLine();
+        }
+    }
+}
