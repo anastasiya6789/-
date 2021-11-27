797c371ea5db3d1bb6e006c79e8154c7f4df5985
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-27 17:01:59 +0300

    Add files via upload

---
+++ 4 задача/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стоимость товара:");
+            double a;
+            double b;
+            double x = Convert.ToInt32(Console.ReadLine());
+
+            if (x >= 500 && x <= 1000)
+            {
+                a = x * 0.97;
+                Console.WriteLine("Цена товара с учетом скидки: " + a);
+            }
+            else if (x > 1000)
+            {
+                b = x * 0.95;
+                Console.WriteLine("Цена товара с учетом скидки: " + b);
+            }
+        }
+    }
+}
