7d434c4a22d85af0eaaa6dfc332a0e20ad8b3f20
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-27 17:39:22 +0300

    Add files via upload

---
+++ 15 задача/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var mas = Array.ConvertAll(Console.ReadLine().Split(" "), s => int.Parse(s));
+            int min = mas[0];
+            int b = 0;
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] < min)
+                {
+                    min = mas[i];
+                    b = i;
+                }
+            }
+            Console.WriteLine($"min = {min} and namber element = {b}");
+        }
+    }
+}
