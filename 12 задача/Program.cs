4df95a04594be5533e6ba4a85923d4b82babad75
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-27 17:37:49 +0300

    Add files via upload

---
+++ 12 задача/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            var n = int.Parse(Console.ReadLine());
+            var array = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+            foreach (int i in array)
+            {
+                if (i == max)
+                {
+                    s++;
+                }
+            }
+            Console.WriteLine($"{max} колличество = {s}");
+        }
+    }
+}
