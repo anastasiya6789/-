fa0348dd9366c93d21efe36fd0a48b58109ca4c3
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-27 16:53:56 +0300

    Add files via upload

---
+++ 2 задача/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, sum = 0, b = -1;
+            Console.WriteLine("Введите предел:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= n; i = i += 2)
+            {
+                a = i;
+                sum += a;
+                b += 1;
+            }
+            Console.WriteLine($"количество от 1 до {n} равно {b} и сумма {sum}");
+        }
+    }
+}
