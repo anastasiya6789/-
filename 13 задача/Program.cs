20437f4595db1cce9f49508db2f06c564f1d9110
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-27 17:38:21 +0300

    Add files via upload

---
+++ 13 задача/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var array = Array.ConvertAll(Console.ReadLine().Split(' '), s => double.Parse(s));
+
+            double sum = 0;
+            foreach (var a in array)
+            {
+                sum += a;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
