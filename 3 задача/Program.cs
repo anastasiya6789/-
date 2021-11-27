001ddbc8e7f2d0741c384033f81705225b0d2bfc
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-27 17:00:29 +0300

    Add files via upload

---
+++ 3 задача/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int sum = 0;
+            Console.WriteLine("Введите с какого числа начнется прогрессия");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите до какого числа будет идти прогрессия");
+            int c = Convert.ToInt32(Console.ReadLine());
+            for (int i = b; i <= c; i++)
+            {
+                a = i;
+                sum += a;
+            }
+            Console.WriteLine($"Сумма {c} членов арифметической прогрессии равна {sum}.");
+        }
+    }
+}
