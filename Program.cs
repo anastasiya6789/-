e374d751402ac37681cfb3cd42de91112f72b3b2
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-27 16:23:22 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,52 @@
 ﻿using System;
-
-namespace _4_задание
+namespace _1_задание
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Введите число:");
-            int n = Convert.ToInt32(Console.ReadLine()), s = 4, i = 1;
-
-            while (i <= n)
-                i += s++;
-            Console.WriteLine(i);
+            Console.Write("Введите номер месяца:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine("February");
+                    break;
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+                case 4:
+                    Console.WriteLine("April");
+                    break;
+                case 5:
+                    Console.WriteLine("May");
+                    break;
+                case 6:
+                    Console.WriteLine("June");
+                    break;
+                case 7:
+                    Console.WriteLine("Jule");
+                    break;
+                case 8:
+                    Console.WriteLine("August");
+                    break;
+                case 9:
+                    Console.WriteLine("September");
+                    break;
+                case 10:
+                    Console.WriteLine("October");
+                    break;
+                case 11:
+                    Console.WriteLine("November");
+                    break;
+                case 12:
+                    Console.WriteLine("December");
+                    break;
+                    
+            }
         }
     }
 }
