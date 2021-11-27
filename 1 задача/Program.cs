4ec8c99941947f28feba298f2a9c77572d7c1ee8
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-27 16:56:29 +0300

    Add files via upload

---
+++ 1 задача/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace work1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину катета треугольника");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = 2;
+            int b = 1;
+            string x = "";
+            string y = " ";
+            string z = "*";
+            string d = "*";
+            Console.WriteLine("*");
+            do
+            {
+                a += 1;
+                Console.WriteLine($"*{x}*");
+                x += y;
+            } while (a < n);
+            while (b != n)
+            {
+                z += d;
+                b += 1;
+            }
+            Console.WriteLine(z);
+        }
+    }
+}
