b7a0cc411ee5cad5a54e5f939223cd697b34fc00
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-27 17:02:49 +0300

    Add files via upload

---
+++ 5 задача/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Что бы увидеть таблицу умножения введите A");
+            string a = Console.ReadLine();
+            string z = "A";
+            int sum;
+            if (a == z)
+            {
+                for (int i = 2; i <= 9; i++)
+                {
+                    for (int j = 1; j <= 10; j++)
+                    {
+                        sum = i * j;
+                        Console.WriteLine($"{i} x {j} = {sum}");
+                    }
+                }
+            }
+            else Console.WriteLine("Видимо эта программа не очень нужна");
+        }
+    }
+}
