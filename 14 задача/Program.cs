2828ae6cc07536340a0b1e5b6a2b112c35137d38
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2021-11-27 17:38:52 +0300

    Add files via upload

---
+++ 14 задача/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt16(Console.ReadLine());
+            var mas = Array.ConvertAll(Console.ReadLine().Split(" "), s => int.Parse(s));
+            int s = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                s = s + mas[i];
+            }
+            s /= n;
+            for (int i = 0; i < n; i++)
+            {
+                if (mas[i] == 0)
+                {
+                    mas[i] = s;
+                }
+                Console.Write($"{mas[i]} ");
+            }
+        }
+    }
+}
